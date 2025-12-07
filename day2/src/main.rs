420a6471fc87bcabd432c6b716801a6d054668cc
Jenia Dysin <genia90@gmail.com>
2025-12-07 23:31:25 +0000

    Day3-part1 complete

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,12 +1,13 @@
 use clap::Parser;
 use std::fs::read_to_string;
+use rayon::prelude::*;
 
 #[derive(Debug, Parser)]
 struct Args {
     input_path: String
 }
 
-fn process_range(start: u64, end: u64) -> u64 {
+fn process_range_part1(start: u64, end: u64) -> u64 {
     let mut total = 0;
     for i in start..end+1 {
         // println!("total: {}; i: {}", total, i);
@@ -14,32 +15,40 @@ fn process_range(start: u64, end: u64) -> u64 {
         let stri_len = stri.len();
         if stri_len % 2 != 0 {
             continue;
-        } else if stri[..stri_len / 2] == stri[(stri_len / 2)..] {
+        } else if stri[..stri_len / 2] == stri[stri_len / 2..] {
             total += i;
         }
     }
     total
 }
 
+fn process_range_part2(start: u64, end: u64) -> u64 {
+    let mut total = 0;
+    for i in start..end+1 {
+        let s = format!("{}", i);
+        let t = format!("{}{}", s, s);
+        if t[1..t.len()-1].contains(s.as_str()) {
+            total += i;
+        }
+
+    }
+    total
+}
+
 fn main() {
     let args = Args::parse();
-    let ranges: Vec<(u64, u64)> = read_to_string(&args.input_path)
+    let total: u64 = read_to_string(&args.input_path)
         .expect("Failed to read input file")
         .trim()
         .split(",")
-        .map(|range| {
+        .collect::<Vec<_>>()
+        .par_iter()
+        .map(|&range| {
             let mut parts = range.split('-');
             let start = parts.next().unwrap().parse().unwrap();
             let end = parts.next().unwrap().parse().unwrap();
-            println!("start: {}; end: {}", start, end);
-            (start, end)
+            process_range_part2(start, end)
         })
-        .collect();
-
-    let mut total = 0;
-    for (start, end) in ranges.iter() {
-        // println!("Range: {}-{}", start, end);
-        total += process_range(*start, *end);
-    }
+        .sum();
     println!("Total: {}", total);
 }
